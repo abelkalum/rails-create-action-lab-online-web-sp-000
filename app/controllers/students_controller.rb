54ed6bc1feb0031925d22e89d60ac4988732f367
Abel Kalum <chumbabel@gmail.com>
2020-08-22 02:13:37 +0000

    Automatically backed up by Learn

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -16,7 +16,7 @@ class StudentsController < ApplicationController
     @student.first_name = params[:first_name]
     @student.last_name = params[:last_name]
     @post.save
-    redirect_to post_path(@post)
+    redirect_to students_path(@student)
   end
 
 end
